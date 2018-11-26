a657805781cd4655bc6d7e8204948ee1191d3ce9
nicholasbrean91 <31775539+nicholasbrean91@users.noreply.github.com>
2018-11-26 14:44:38 -0500

    Create Bike.java

---
+++ Bike.java
@@ -0,0 +1,11 @@
+
+public class Bike extends Vehicle{
+
+	private static int _wheels;
+	
+	public Bike (String lPlateNum,
+				 String colour,
+				 int wheels) {
+		super(lPlateNum, colour, wheels);
+	}
+}
