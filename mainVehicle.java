74561520c1874548464600e88d70e3db0e31d3d2
nicholasbrean91 <31775539+nicholasbrean91@users.noreply.github.com>
2018-11-27 14:49:53 -0500

    Update mainVehicle.java

--- mainVehicle.java
+++ mainVehicle.java
@@ -1,7 +1,3 @@
-//Created By Nicholas Brean
-//Created on Nov 2018
-//Created to return the # wheels in various classes using Inheritance.
-
 import java.util.Scanner;
 public class mainVehicle {
 	public static void main(String args[]){
@@ -35,12 +31,35 @@ public class mainVehicle {
 			wheels = userInput.nextInt();
 			System.out.println("Enter the number of Doors you would like your car to have: ");
 			numOfDoors = userInput.nextInt();
+	
+			
 			
 			Vehicle obj = new Vehicle(lPlateNum, colour, wheels);
 			int numOfWheels = obj.wheels();
 			
+			System.out.println("Enter the speed you want to increase from 0: ");
+			int increase = userInput.nextInt();
+			System.out.println("Enter the speed you want to decrease from " + obj.accelerate(increase)  + ": ");
+			int decrease = userInput.nextInt();
+			
+			if (obj.accelerate(increase) < 0) {
+				System.out.println("You are not allowed to go that fast, the max is 270");
+				System.exit(0);
+			}
+			
+			if (obj.brake(decrease, obj.accelerate(increase)) == -1) {
+				System.out.println("You are not allowed to go that slow, the lowest is 0");
+				System.exit(0);
+			}
+			
+			
 			Truck truckObj = new Truck(lPlateNum, colour, numOfWheels, numOfDoors);
-			System.out.println("-=-=-=Truck=-=-=-\nThe licience plate num is " + truckObj.lPlateNum() + "\nThe colour of the car is " + truckObj.colour() + "\nThe num of wheels is " +  truckObj.wheels() + "\nThe # of doors the car has is " + truckObj.numOfDoors()  );
+			System.out.println("-=-=-=Truck=-=-=-\nThe licience plate num is " + truckObj.lPlateNum() + 
+								"\nThe colour of the car is " + truckObj.colour() + 
+								"\nThe num of wheels is " +  truckObj.wheels() + 
+								"\nThe # of doors the car has is " + truckObj.numOfDoors() + 
+								"\nThe speed went from zero to " + obj.accelerate(increase) + 
+								"\nThen you decreased from " + obj.accelerate(increase) + " to " + obj.brake(decrease, obj.accelerate(increase))  );
 			
 		}else {
 			System.out.println("Please enter one of the following inputs 'bike' or 'truck'.");
